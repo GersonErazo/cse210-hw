6f30836416f6f8ceddbe93ef2a8b63f7b096294d
GersonErazo <era24001@byui.edu>
2024-04-26 19:33:34 -0600

    modified:   csharp-prep/Prep1/Program.cs 	modified:   csharp-prep/Prep2/Program.cs 	modified:   csharp-prep/Prep3/Program.cs 	modified:   csharp-prep/Prep4/Program.cs 	modified:   csharp-prep/Prep5/Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int p = int.Parse(answer);
+
+        string letter = "";
+
+        if (p >= 90)
+        {
+            letter = "A";
+        }
+        else if (p >= 80)
+        {
+            letter = "B";
+        }
+        else if (p >= 70)
+        {
+            letter = "C";
+        }
+        else if (p >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (p >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You don't passed. Better luck next time!");
+        }
     }
 }
